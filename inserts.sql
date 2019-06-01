af4114de645909e546f494161c92868704518a48
Diogo Paulo Esteves Ramos <a39954@alunos.ipb.pt>
2019-06-01 21:46:18 +0100

    Mudanças menores na DB, tenhem que voltar a sincronizar a BD no mysql

---
+++ inserts.sql
@@ -0,0 +1,14 @@
+INSERT INTO utilizador (Nome, Email, Endereço, Tipo_de_Utilizador, NIF, Password )
+	VALUES("Diogo Ramos", "dperss@gmail.com" , "rua do desespero", "1", "654545", "ndhghx");
+INSERT INTO utilizador (Nome, Email, Endereço, Tipo_de_Utilizador, NIF, Password )
+	VALUES("Rosana ", "Rosana@gmail.com" , "rua da Rosana", "1", "654545", "ndhghx");
+---------------------------------------------------
+
+INSERT INTO categoria (Nome)
+	VALUES("Portas");
+INSERT INTO categoria (Nome)
+	VALUES("Janelas");
+INSERT INTO categoria (Nome)
+	VALUES("Portões");
+INSERT INTO categoria (Nome)
+	VALUES("Grades");
